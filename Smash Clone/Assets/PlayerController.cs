340a0b157cc929ed553c024fe1ad67760e4f39e4
Cameron burridge <cmburridge4022@gmail.com>
2020-10-05 23:51:30 -0600

    Added basic movement and downloaded stage

---
+++ Smash Clone/Assets/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 movement;
+
+    public float gravity = -9.81f, jumpForce = 10f;
+    private float yVar;
+
+    public FloatData moveSpeed, fastSpeed;
+    public FloatData normalSpeed;
+
+    public FloatData MoveSpeed => moveSpeed;
+
+    public IntData playerJumpCount;
+    private int jumpCount;
+    
+    private void Start()
+    {
+        moveSpeed = normalSpeed;
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        {
+            var vInput = Input.GetAxis("Horizontal")*moveSpeed.value;
+            movement.Set(vInput,yVar,0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveSpeed = fastSpeed;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed = normalSpeed;
+        }
+        
+        yVar += gravity*Time.deltaTime;
+
+        if (controller.isGrounded && movement.y < 0)
+        {
+            yVar = -1f;
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount< playerJumpCount.value)
+        {
+            yVar = jumpForce;
+            jumpCount++;
+        }
+
+        movement = transform.TransformDirection(movement);
+        controller.Move(movement * Time.deltaTime);
+        
+    }
+}
